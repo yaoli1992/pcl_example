a95e900566fe9f6267afb5ac5241a2d77dc9ffe5
yaoli1992 <920177957@qq.com>
2017-08-20 11:43:32 +0800

    first commit

---
+++ ch3_3/build/CMakeFiles/octree_search.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/octree_search.dir/octree_search.cpp.o"
+  "octree_search.pdb"
+  "octree_search"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/octree_search.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
